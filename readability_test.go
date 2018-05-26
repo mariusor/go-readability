6052be8a805a28d45dd83fe1f9a985657a1a74f8
Marius Orcsik <marius@habarnam.ro>
2018-05-26 19:03:11 +0200

    Revert "Merge pull request #1 from mariusor/old-changes"
    This reverts commit 561c8adfa363718cccbb435c1b0a93b1758b34bd, reversing
    changes made to 055ab65838abc2a36bfafafdcca65846ede4bf13.

--- readability_test.go
+++ readability_test.go
@@ -1,10 +1,9 @@
 package readability
 
 import (
-	"bufio"
 	"io/ioutil"
-	"log"
-	"os"
+	//"log"
+	//"os"
 	"strings"
 	"testing"
 )
@@ -14,7 +13,7 @@ type expectedOutput struct {
 	excludedFragments []string
 }
 
-func _TestGeneralFunctionality(t *testing.T) {
+func TestGeneralFunctionality(t *testing.T) {
 	html := `<html><head><title>title!</title></head><body><div><p>Some content</p></div></body>`
 	doc, err := NewDocument(html)
 	if err != nil {
@@ -43,7 +42,7 @@ func _TestGeneralFunctionality(t *testing.T) {
 	}
 }
 
-func _TestIgnoringSidebars(t *testing.T) {
+func TestIgnoringSidebars(t *testing.T) {
 	html := `html><head><title>title!</title></head><body><div><p>Some content</p></div><div class='sidebar'><p>sidebar<p></div></body>`
 	doc, err := NewDocument(html)
 	if err != nil {
@@ -59,7 +58,7 @@ func _TestIgnoringSidebars(t *testing.T) {
 	}
 }
 
-func _TestInsertSpaceForBlockElements(t *testing.T) {
+func TestInsertSpaceForBlockElements(t *testing.T) {
 	html := `<html><head><title>title!</title></head>
           <body>
             <div>
@@ -79,7 +78,7 @@ func _TestInsertSpaceForBlockElements(t *testing.T) {
 	}
 }
 
-func _TestOutputForWellKnownDocuments(t *testing.T) {
+func TestOutputForWellKnownDocuments(t *testing.T) {
 	inputs := map[string]*expectedOutput{
 		"blogpost_with_links.html": {
 			requiredFragments: []string{
@@ -156,6 +155,7 @@ func _TestOutputForWellKnownDocuments(t *testing.T) {
 		},
 	}
 
+	//Logger = log.New(os.Stdout, "[readability] ", log.LstdFlags)
 	for file, expectedOutput := range inputs {
 		bytes, err := ioutil.ReadFile("test_fixtures/" + file)
 		if err != nil {
@@ -184,39 +184,6 @@ func _TestOutputForWellKnownDocuments(t *testing.T) {
 				t.Errorf("Did not expect content %q to contain %q", file, excluded)
 			}
 		}
-	}
-}
-
-func TestOutput(t *testing.T) {
-	Logger = log.New(os.Stdout, "[readability] ", log.LstdFlags)
-	inputs := []string{"parahumans-chap1.html"}
-
-	for _, file := range inputs {
-		bytes, err := ioutil.ReadFile("test_fixtures/" + file)
-		if err != nil {
-			t.Fatal("Unable to read file test_fixtures/", file, err)
-		}
-
-		input := string(bytes)
-		doc, err1 := NewDocument(input)
-		if err1 != nil {
-			t.Fatal("Unable to create document", err1)
-		}
-
-		content := doc.Content()
-
-		f, err2 := os.Create("test_fixtures/readable-" + file)
-		if err2 != nil {
-			t.Fatal("Unable to create document", err2)
-		}
-
-		w := bufio.NewWriter(f)
-		_, err3 := w.WriteString(content)
-		if err3 != nil {
-			t.Fatal("Unable to write document", err3)
-		}
-
-		w.Flush()
 	}
 }
 
